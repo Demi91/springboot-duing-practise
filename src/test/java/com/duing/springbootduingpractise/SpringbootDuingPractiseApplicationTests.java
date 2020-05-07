87bbcdc903b301acf72d2d93f5a746a642af2d11
Demi91 <lanluo_bingzi@126.com>
2020-05-07 19:09:02 +0800

    init

---
+++ src/test/java/com/duing/springbootduingpractise/SpringbootDuingPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duing.springbootduingpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDuingPractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
